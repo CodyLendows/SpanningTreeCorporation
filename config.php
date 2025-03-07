0c2c5ddd7ff7810cdc96e7a59046fb8b751accf9
salem <salem@localhost>
2025-03-07 09:29:56 +0100

    Add variables in config, create .gitignore

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+
+// Remote connection details
+$remote_host = '207.127.89.39';
+$remote_port = 22; // default SSH port
+
+$username = 'salem';
+$password = '2k0o4m8a6e3a';
+?>
